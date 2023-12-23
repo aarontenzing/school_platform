5faa0ba4e3d40d0523aae4467910cd79bb9d673a
aarontenzing <aarontenzing@gmail.com>
2023-12-23 18:58:35 +0100

    databank login

--- platform/src/main/java/com/app/platform/controllers/Config.java
+++ platform/src/main/java/com/app/platform/controllers/Config.java
@@ -1,12 +1,22 @@
 package com.app.platform.controllers;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class Config {
+	
+	@Autowired
+	DataSource dataSource;
 	
 	@Bean
 	public SecurityFilterChain beveilig(HttpSecurity http) throws Exception 
@@ -19,9 +29,25 @@ public class Config {
 						.loginPage("/login")
 						.defaultSuccessUrl("/home")
 						.permitAll()
-				);
+				)
+				.logout(logout -> logout
+						.logoutSuccessUrl("/login"));
 		
 		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	public void dbauth(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery("select username,passwd,1 from users where username = ?")
+		.authoritiesByUsernameQuery("select username,authority from authorities where username = ?");
 	}
 
 }
